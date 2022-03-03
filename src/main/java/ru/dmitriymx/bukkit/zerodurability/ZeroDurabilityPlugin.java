c8bc72957148b5575426089de451db002af87759
DmitriyMX <dimon550@gmail.com>
2022-03-03 20:50:16 +0300

    use Spigot plugin annotations

--- src/main/java/ru/dmitriymx/bukkit/zerodurability/ZeroDurabilityPlugin.java
+++ src/main/java/ru/dmitriymx/bukkit/zerodurability/ZeroDurabilityPlugin.java
@@ -11,9 +11,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 import java.util.stream.Stream;
 
+@Plugin(name = "ZeroDurabillity", version = "1.0")
+@Author("DmitriyMX")
+@Website("https://gitlab.com/DmitriyMX/zerodurability")
 public class ZeroDurabilityPlugin extends JavaPlugin implements Listener {
     private FileConfiguration configuration;
 
